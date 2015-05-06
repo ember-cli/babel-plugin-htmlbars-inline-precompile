9897affe5822b85f53b2b70fcd0a49a4d6bd7057
pangratz <cmueller.418@gmail.com>
2015-05-06 11:42:32 +0200

    Rename state parameter to file
    The passed parameter is a babel file object.

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ module.exports = function(precompile) {
     var t = babel.types;
 
     return new babel.Transformer('htmlbars-inline-precompile', {
-      ImportDeclaration: function(node, parent, scope, state) {
+      ImportDeclaration: function(node, parent, scope, file) {
         if (t.isLiteral(node.source, { value: "htmlbars-inline-precompile" })) {
           var first = node.specifiers && node.specifiers[0];
           if (t.isImportDefaultSpecifier(first)) {
-            state.importSpecifier = first.local.name;
+            file.importSpecifier = first.local.name;
           } else {
-            var input = state.code;
+            var input = file.code;
             var usedImportStatement = input.slice(node.start, node.end);
             var msg = "Only `import hbs from 'htmlbars-inline-precompile'` is supported. You used: `" + usedImportStatement + "`";
-            throw state.errorWithNode(node, msg);
+            throw file.errorWithNode(node, msg);
           }
 
           this.remove();
         }
       },
 
-      TaggedTemplateExpression: function(node, parent, scope, state) {
-        if (t.isIdentifier(node.tag, { name: state.importSpecifier })) {
+      TaggedTemplateExpression: function(node, parent, scope, file) {
+        if (t.isIdentifier(node.tag, { name: file.importSpecifier })) {
           var template = node.quasi.quasis.map(function(quasi) {
             return quasi.value.cooked;
           }).join("");
