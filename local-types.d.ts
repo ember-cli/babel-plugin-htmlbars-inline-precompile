0a44d96ca0e4840c6dc6d442c0983a29ef91da16
Edward Faulkner <ef@alum.mit.edu>
2021-07-07 15:47:46 -0400

    dropping unused dep

--- local-types.d.ts
+++ local-types.d.ts
@@ -1,15 +1,3 @@
-declare module 'parse-static-imports' {
-  export interface Import {
-    moduleName: string;
-    starImport: string;
-    namedImports: { name: string; alias: string }[];
-    defaultImport: string;
-    sideEffectOnly: boolean;
-  }
-
-  export default function parseStaticImports(code: string): Import[];
-}
-
 declare module '@babel/plugin-transform-template-literals' {}
 declare module '@babel/plugin-transform-modules-amd' {}
 declare module '@babel/plugin-transform-unicode-escapes' {}
